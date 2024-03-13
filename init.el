a9f4c394cfefb250c2ec54acb31b9d0f725facbc
David <dave.feller@gmail.com>
2024-03-12 19:34:29 -0600

    set display-line-numbers-width 2

--- init.el
+++ init.el
@@ -237,7 +237,8 @@
 ;;;; line numbers
 
 (require 'display-line-numbers)
-(setq display-line-numbers-type 'visual
+(setq display-line-numbers-width 2
+      display-line-numbers-type 'visual
       display-line-numbers-current-absolute nil)
 
 (add-hook 'prog-mode-hook #'display-line-numbers-mode)
